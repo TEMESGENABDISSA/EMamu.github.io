526d7af73859c07c10552b55f107c68cea632ff3
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2023-12-13 19:46:58 +0300

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,17 @@
 'use strict';
-/**
- * navbar toggle in mobile
- */
-const /**{node element}*/ $navbar=document.querySelector("[data-navbar]");
-const /**{node element}*/ $navToggler=document.querySelector("[data-nav-toggler]");
+
+const $navbar=document.querySelector("[data-navbar]");
+const  $navToggler=document.querySelector("[data-nav-toggler]");
 $navToggler.addEventListener("click",()=>$navbar.classList.toggle("active"));
 
-/**
- * header scroll state
- */
-const /**{node element}*/ $header=document.querySelector("[ data-header]");
+
+const  $header=document.querySelector("[ data-header]");
 window.addEventListener("scroll", o  => {
 $header.classList[window.scrollY>50? "add":"remove"]("active");
 });
 
-/**
- * add to favorite button toggle favorite filachu
- */
-const /**{node element}*/ $toggleBtns=document.querySelectorAll("[ data-toggle-btn]");
+
+const $toggleBtns=document.querySelectorAll("[ data-toggle-btn]");
 $toggleBtns.forEach( $toggleBtns =>{
     $toggleBtns.addEventListener("click", ()=> {
         $toggleBtns.classList.toggle("active");
